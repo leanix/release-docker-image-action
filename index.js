64b031fbc2561dc85691b18de3c7606fc013bfd9
Per Bernhardt <per.bernhardt@leanix.net>
2020-02-19 07:57:00 +0100

    HOOK-1333 WIP

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const git = require('simple-git')('.');
 
+console.log('hello world');
+
 git.tag(
     ["--merged", process.env.GITHUB_REF],
     (tags) => console.log(tags)
