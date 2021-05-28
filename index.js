08d2f2c4a9df0356015566fa5702dfff3751d102
philippglock <philipp.glock@leanix.net>
2021-05-28 12:09:47 +0200

    SENSE-3494 fix cache options

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const fs = require('fs');
             if (enableCache) {
                 const builderArgs = ['--builder', builder];
                 const nameWithCurrentVersion = name + ":" + normalisedBranch + "-" + currentVersion;
-                const cachingArgs = ["--caching-from", "type=registry," + nameWithCurrentVersion, "--caching-to", "type=inline"];
+                const cachingArgs = ["--cache-from", "type=registry," + nameWithCurrentVersion, "--cache-to", "type=inline"];
                 await exec.exec('docker', ['buildx', 'build', ...builderArgs, ...cachingArgs, '-t', nameWithVersion, ...dockerfile_param, path], options);
             }
             else {
